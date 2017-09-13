a7333620e8e1d6a80e49a5a3a6fdcf286e6ef4a6
Glass Fox <daronb83@gmail.com>
2017-09-13 14:01:41 -0600

    last straw

--- Effect.java
+++ Effect.java
@@ -81,7 +81,11 @@ public class Effect {
           temp.pushRGB(image.getPixel(w,h).getRed(), image.getPixel(w,h).getGreen(), image.getPixel(w,h).getBlue());
         }
         else {
-          for (int k = 0; k < thisBlur - 1; k++){
+          for (int k = 0; k < thisBlur; k++){
+            if (w == 0 && h == 0){
+              System.out.print("(" + w + "," + h + ") ");
+              System.out.println(image.getPixel(w+k,h).getRed() + "," + image.getPixel(w+k,h).getGreen() + "," + image.getPixel(w+k,h).getBlue());
+            }
             averageRed += image.getPixel(w+k,h).getRed();
             averageGreen += image.getPixel(w+k,h).getGreen();
             averageBlue += image.getPixel(w+k,h).getBlue();
